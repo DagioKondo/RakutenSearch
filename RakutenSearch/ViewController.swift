c1376bdb685668d207d48172d98e190e15f6f56e
DaigoKondo <daigo.soccer.0106@icloud.com>
2023-02-18 00:18:45 +0900

    Initial Commit

---
+++ RakutenSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RakutenSearch
+//
+//  Created by 近藤大伍 on 2023/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
